ee6cfe4b09fbafb02715b21cc888345ea464463c
Richard Wells <richard.wells1987@hotmail.co.uk>
2023-04-14 16:55:03 +0100

    api stuff

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,28 +1,43 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "This is a first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "This is a second meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
-
 const AllMeetupsPage = () => {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+  const [loading, setLoading] = useState(true);
+  const [meetups, setMeetups] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://react-8675d-default-rtdb.europe-west1.firebasedatabase.app/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+
+        setMeetups(meetups);
+        setLoading(false);
+      });
+  }, [meetups]);
+
+  if (loading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  return <MeetupList meetups={meetups} />;
 };
 
 export default AllMeetupsPage;
--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,7 +1,30 @@
-import React from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupsPage = () => {
-  return <div>NewMeetups</div>;
+  const history = useHistory();
+
+  const handleNewMeetup = (formData) => {
+    fetch(
+      "https://react-8675d-default-rtdb.europe-west1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(formData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  };
+
+  return (
+    <section>
+      <h1>New Meetups Page</h1>
+      <NewMeetupForm onSubmit={handleNewMeetup} />
+    </section>
+  );
 };
 
 export default NewMeetupsPage;
